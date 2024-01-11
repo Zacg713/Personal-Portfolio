5884204ee733c0582263ba76c6d7731b550df5a1
Zacg713 <136399468+Zacg713@users.noreply.github.com>
2024-01-11 11:17:47 -0600

    changed hover animations for nav buttons and fixed issue with link being able to be clicked while not on screen

--- script.js
+++ script.js
@@ -8,46 +8,46 @@ const certificates = document.getElementById("certificates");
 aboutme.classList.add("fade-in");
 
 function toggleAboutMe() {
-    skills.classList.remove("fade-in");
-    projects.classList.remove("fade-in");
-    experience.classList.remove("fade-in");
-    contact.classList.remove("fade-in");
-    certificates.classList.remove("fade-in");
-    aboutme.classList.add("fade-in");  
+    skills.style.visibility = "hidden";
+    projects.style.visibility = "hidden";
+    experience.style.visibility = "hidden";
+    contact.style.visibility = "hidden";
+    certificates.style.visibility = "hidden";
+    aboutme.style.visibility = "visible";  
 }
 
 function toggleSkills() {
-    aboutme.classList.remove("fade-in");
-    projects.classList.remove("fade-in");
-    experience.classList.remove("fade-in");
-    contact.classList.remove("fade-in");
-    certificates.classList.remove("fade-in");
-    skills.classList.add("fade-in");
+    aboutme.style.visibility = "hidden";
+    projects.style.visibility = "hidden";
+    experience.style.visibility = "hidden";
+    contact.style.visibility = "hidden";
+    certificates.style.visibility = "hidden";
+    skills.style.visibility = "visible";
 }
 
 function toggleProjects() {
-    aboutme.classList.remove("fade-in");
-    skills.classList.remove("fade-in");
-    experience.classList.remove("fade-in");
-    contact.classList.remove("fade-in");
-    certificates.classList.remove("fade-in");
-    projects.classList.add("fade-in");
+    skills.style.visibility = "hidden";
+    aboutme.style.visibility = "hidden";
+    experience.style.visibility = "hidden";
+    contact.style.visibility = "hidden";
+    certificates.style.visibility = "hidden";
+    projects.style.visibility = "visible";
 }
 
 function toggleExperience() {
-    aboutme.classList.remove("fade-in");
-    skills.classList.remove("fade-in");
-    projects.classList.remove("fade-in");
-    contact.classList.remove("fade-in");
-    experience.classList.add("fade-in");
-    certificates.classList.add("fade-in");
+    skills.style.visibility = "hidden";
+    projects.style.visibility = "hidden";
+    aboutme.style.visibility = "hidden";
+    contact.style.visibility = "hidden";
+    certificates.style.visibility = "visible";
+    experience.style.visibility = "visible";
 }
 
 function toggleContact() {
-    aboutme.classList.remove("fade-in");
-    skills.classList.remove("fade-in");
-    projects.classList.remove("fade-in");
-    experience.classList.remove("fade-in");
-    certificates.classList.remove("fade-in");
-    contact.classList.add("fade-in");
+    skills.style.visibility = "hidden";
+    projects.style.visibility = "hidden";
+    experience.style.visibility = "hidden";
+    aboutme.style.visibility = "hidden";
+    certificates.style.visibility = "hidden";
+    contact.style.visibility = "visible";
 }
